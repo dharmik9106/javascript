696e8b79853d1ab0c3a26cfb5f536052c23c363b
Dharmik Ahir <sangadharmik78@gmail.com>
2025-08-22 19:22:35 +0530

    operation

---
+++ 02 JS/index.js
@@ -0,0 +1,78 @@
+// arithmetic operation 
+// let a=5;
+// let b=3;
+ 
+
+// console.log ("a + B " ,  a + b );
+// console.log ("a - B " ,  a - b );
+// console.log ("a * B " ,  a * b );
+// console.log ("a / B " ,  a / b );
+// console.log ("a % B " ,  a % b );
+// console.log ("a ** B " ,  a ** b );
+
+
+// Unary Operation
+
+// let a = 6;
+// let b = 4;
+
+// console.log ("a = " ,a ,"& b = " , b);
+
+// console.log( "a++ ", a++);
+// console.log( "a++ ", a);
+
+// // Assingment Operation 
+ 
+// let a = 5;
+// let b = 2;
+
+// a+= 4 ; // a =  a + 4
+// console. log ( " a = " , a);
+
+// a-= 4 ; // a =  a - 4
+// console. log ( " a = " , a);
+
+// a*= 4 ; // a =  a * 4
+// console. log ( " a = " , a);
+
+// a/= 4 ; // a =  a / 4
+// console. log ( " a = " , a);
+
+// a%= 4 ; // a =  a % 4
+// console. log ( " a = " , a);
+
+// a **= 4 ; // a =  a ** 4
+// console. log ( " a = " , a);
+
+
+
+// Comparison Operaters
+
+
+// let a = 5;
+// let b = 5;
+
+// console.log ("5==5" , a==b); //false
+// console.log ("5!=2" , a!=b); //false
+// console.log ("5===5" , a===b); //false
+// console.log ("5!==5" , a!==b); //false
+// console.log ("5>=5" , a>=b); //false
+// console.log ("5<5" , a<b); //false
+
+
+// Logical Operation 
+
+// let a = 4;
+// let b = 7;
+
+
+// console.log(" !(a>b) =",! a===6 );  
+
+// conditional statment 
+
+let  age = 19
+
+if (age > 18 ){
+    console.log ("you can vote");
+
+}
