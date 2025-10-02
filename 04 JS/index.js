b7b2f664cfddab4fffdda8fae8face95efc81938
Dharmik Ahir <sangadharmik78@gmail.com>
2025-10-02 17:50:06 +0530

    variable

---
+++ 04 JS/index.js
@@ -0,0 +1,18 @@
+
+let name = "Dharmik";
+
+
+function sayName(){
+
+    var person= "Dharmik Sanga";
+    console.log("person -> ", person);  
+}
+ 
+sayName ();
+
+
+let num;
+
+num = 10;
+
+console.log(num);
