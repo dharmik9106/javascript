e044bac6528ee2534157e40424eab92b3b63d9fd
Dharmik Ahir <sangadharmik78@gmail.com>
2025-10-07 10:59:52 +0530

    Control Structure

---
+++ 07 JS CONTROL STRUCTURE/loop.js
@@ -0,0 +1,12 @@
+
+
+for (let i= 1; i<1000; i++){
+    // console.log(i);
+
+} 
+
+const  students =["vivek", "vraj" ,"utsav" , "krushnal" , "amit"];
+
+for (let i =0; i<students.length; i++){
+    console.log(students[i]);
+};
