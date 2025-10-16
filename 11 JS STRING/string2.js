0cfa97e2830f5be96d37cd9037cff6958872f374
Dharmik Ahir <sangadharmik78@gmail.com>
2025-10-16 11:59:27 +0530

    STRING METHOD

--- 11 JS STRING/string2.js
+++ 11 JS STRING/string2.js
@@ -1,8 +1,84 @@
+// match  method
 
+let greeting = "hello goodMorning";
 
-let name = "Hello Dharmik Sanga ";
+console.log(greeting.match("good"));
 
-console.log (name.match("Sanga"));
+// if not match
 
+console.log("if not match", greeting.match("abcd"));
 
-console.log("if not match:", name .match("abcd"));
+// repeat example
+
+let text = "abcd";
+
+console.log(text.repeat(3));
+
+// replace
+
+text = "abcdabcda";
+
+console.log(text.replace("a", "A"));
+
+// replace all
+
+console.log(text.replaceAll("a", "A"));
+
+const text2 = "yxxyz";
+
+console.log(text2.indexOf("x", 2));
+
+console.log(text2.search("x"));
+
+console.log("text", greeting);
+
+console.log(greeting.slice(3));
+
+console.log(greeting.slice(2, 8));
+
+console.log(greeting.slice(-1));
+
+let alphabet = "abcdefghi";
+
+console.log(alphabet.split(" "));
+
+console.log(alphabet.split(""));
+
+console.log(alphabet.split("e"));
+
+// starts with
+
+console.log("starts with", alphabet.startsWith("a"));
+console.log("starts with", alphabet.startsWith("z"));
+
+// substring
+
+console.log("substring", alphabet.substring(3));
+
+console.log("substring ex", alphabet.substring(2, 5));
+
+console.log("substring ex", alphabet.substring(5, 2));
+
+console.log("substring", alphabet.substring(-1));
+
+console.log("substring", alphabet.substring(-5, 5));
+
+// uppercase
+
+console.log("uppercase", alphabet.toUpperCase());
+
+alphabet = "ABCDEFGHI";
+
+console.log("lower case", alphabet.toLowerCase());
+
+// trim
+
+alphabet = "               abcdefghi                  ";
+
+console.log("length of alphabet", alphabet.length);
+
+console.log("both sides", alphabet.trim().length);
+
+console.log("only starting", alphabet.trimStart().length);
+
+console.log("only ending", alphabet.trimEnd().length);
