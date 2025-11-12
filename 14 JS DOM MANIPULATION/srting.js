20f6b62fb34abfa5899500a0b2f5268d31fae61c
Dharmik Ahir <sangadharmik78@gmail.com>
2025-11-12 11:29:43 +0530

    DOM MANIPULATION

---
+++ 14 JS DOM MANIPULATION/srting.js
@@ -0,0 +1,5 @@
+
+document.getElementById("block").textContent ="this is part changed using script";
+
+
+document.getElementsByClassName("block2")[0].innerHTML ="<h1>this is 2 class paragraph</h1>"; 
