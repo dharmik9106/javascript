bdef62af16bf167eab82347ad3c0b82a867a38b9
Dharmik Ahir <sangadharmik78@gmail.com>
2025-12-08 18:09:33 +0530

    Movie API

---
+++ 20 Fetch API/Movie Api/index.js
@@ -0,0 +1,53 @@
+document.getElementById("form").addEventListener("submit",async(e)=>{
+    e.preventDefault()
+
+    try {
+
+        const movieName = document.getElementById("input").value
+
+
+        const res = await fetch(`https://www.omdbapi.com/?apikey=thewdb&s=${movieName}`)
+
+    
+        const data = await res.json()  
+        
+        console.log(data)
+
+        document.getElementById("input").value=""
+
+        if(data.Response === "True" ){
+        const container =  document.getElementById("container")
+
+         data.Search.forEach((movie)=>{
+
+            const image = document.createElement("img")
+
+            image.src = movie.Poster;
+            image.title= movie.Title;
+            image.style.width="200px";
+            image.style.margin="10px"
+
+            
+         container.appendChild(image)
+
+         })
+
+          
+        }else {
+
+            const container = document.getElementById("container")
+
+            
+            container.innerHTML = `<h1>No matched found </h1>`
+
+            throw new Error("failed to fetch movie")
+
+        }
+   
+    } catch (error) {
+
+        console.log(error)
+        
+    }
+
+})
