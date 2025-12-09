8726cd99e9870cde80b4fd92ea2b2baf2bb9fe6f
Dharmik Ahir <sangadharmik78@gmail.com>
2025-12-09 16:56:21 +0530

    Parameter

---
+++ 22 Parameter/index.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+const game = (game) =>{
+
+    console.log(game)
+}
+
+game("food court")
+
+
+// 
+
+
+const game2 = (game) =>{
+    console.log("i am eating",game)
+}
+
+game2("chips")
+
+
+
+const game3 = (game="burger") =>{
+    console.log("i am eating",game)
+}
+
+game3()
+
+
+const game4 = (game="pizza") =>{
+    console.log("i am eating",game)
+}
+
+game3("dosa")
