f4d9c1b2ec422d54d22e5ff209d190a02dd149d0
Dharmik Ahir <sangadharmik78@gmail.com>
2025-12-30 17:31:36 +0530

    JQUARY PLUG IN

---
+++ 25_JS_CAROUSEL_OWL/counter/script.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:20,
+        nav:true,
+        dots:true,
+        center:true,
+        autoplay:true,
+        autoplayTimeout:2500,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+
+});
